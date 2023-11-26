ff6f2c8f0c986ba087c58af2cf20cfafc96bb529
Ludvig Ward <ludvig@upward.se>
2023-11-26 15:12:22 +0100

    Updated test condition in main class

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        var guesser = new Guesser(0, 100);
+        var guesser = new Guesser(0, 10);
         guesser.start();
     }
 }
